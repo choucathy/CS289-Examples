71ecc081278a5dde58d55b6ec8639ebfd0448262
choucathy <cathy730419@gmail.com>
2017-09-11 22:07:03 +0800

    新增第七堂範例

---
+++ EasyMathLibrary/EasyMathLibrary/Vector.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMathLibrary
+{
+    public class Vector
+    {
+        private static Random random;
+        public static int TotalVectors;
+
+        #region 欄位與屬性
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Max { get; set; }
+        public double Min { get; set; }
+
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(X * X + Y * Y);
+            }
+        }
+
+        #endregion 欄位與屬性
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+            TotalVectors++;
+        }
+
+        public Vector()
+        {
+            TotalVectors++;
+        }
+
+        public static Vector Generate(double min, double max)
+        {
+            if (Vector.random == null)
+                Vector.random = new Random();
+
+            Random random = new Random();
+            double x = random.NextDouble(min, max);
+            double y = random.NextDouble(min, max);
+            Vector v = new Vector(x, y);
+            return v;
+        }
+
+        public Vector ScaleX(double factor)
+
+        {
+            X *= factor;
+            return this;
+        }
+
+        public Vector ScaleY(double factor)
+
+        {
+            Y *= factor;
+            return this;
+        }
+
+        public Vector Scale(double factorx, double factory)
+        {
+            return this.ScaleX(factorx).ScaleY(factory);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("(x,y) ={0},{1}", X, Y);
+        }
+    }
+}
---
+++ EasyMathLibrary/TestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMathLibrary;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Random random = new Random();
+            //random.NextDouble(-10, 11);
+            //Console.WriteLine(random.NextDouble(-10, 11));
+            //Vector v = new Vector();
+            //v.Generate(-10, 10);
+            //Console.WriteLine(v.Generate(-10, 10).Generate(-10, 10).X);
+            //Console.WriteLine(v);
+            //Console.WriteLine(v.Scale(2, 3));
+
+            Console.WriteLine();
+
+            for (int count = 1; count <= 10; count++)
+            {
+                Console.WriteLine(Vector.Generate(-100, 100));
+            }
+        }
+    }
+}
